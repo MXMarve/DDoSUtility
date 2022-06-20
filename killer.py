1799bf1a53e5939b2e42be447d5d1a169456b55c
MXMarve <95297108+MXMarve@users.noreply.github.com>
2022-06-19 20:20:52 -0500

    Add files via upload

--- killer.py
+++ killer.py
@@ -1,3 +1,5 @@
 import os
 ip = input("URL: ")
-os.system("python3 attacker.py "+ip)
+count = input("How hard to hit: 1-50 (Only go more than 3 if your computer supports it): ")
+for x in range(int(count)):
+  os.system("python3 attacker.py "+ip)
